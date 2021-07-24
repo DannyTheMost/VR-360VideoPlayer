24a7ee51b4a551a049caf7409145c4509bfbdf83
DannyTheMost <danny.garci@gmail.com>
2021-07-24 21:11:46 +0200

    Terminado
    Hay un problema con el icono de load

--- Assets/_360VideoPlayer/Scripts/Feedback.cs
+++ Assets/_360VideoPlayer/Scripts/Feedback.cs
@@ -2,5 +2,39 @@
 
 public class Feedback : MonoBehaviour
 {
-  
+    public float distance = 2.5f;
+    public VideoManager videoManager = null;
+    [Header("Icons")]
+    public Sprite pause = null;
+    public Sprite load = null;
+    private SpriteRenderer spriteRenderer = null;
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        SetupWithCamera();
+        videoManager.onPause.AddListener(TogglePause);
+        videoManager.onLoad.AddListener(ToggleLoad);
+    }
+    private void OnDestroy()
+    {
+        videoManager.onPause.RemoveListener(TogglePause);
+        videoManager.onLoad.RemoveListener(ToggleLoad);
+    }
+    private void SetupWithCamera()
+    {
+        transform.parent = Camera.main.transform;
+        transform.localRotation = Quaternion.identity;
+        transform.localPosition = new Vector3(0,0,distance);
+    }
+    private void TogglePause(bool isPaused)
+    {
+        spriteRenderer.sprite = pause;
+        spriteRenderer.enabled = isPaused;
+    }
+    private void ToggleLoad(bool isLoaded)
+    {
+        spriteRenderer.sprite = load;
+        spriteRenderer.enabled = !isLoaded;
+    }
+
 }
